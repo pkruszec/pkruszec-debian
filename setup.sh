0d8e490c79c954d0075860ee5f2711694fe175c3
Paweł Kruszec <83838979+poiu530@users.noreply.github.com>
2024-07-20 16:24:29 +0200

    Don't download Iosevka

--- setup.sh
+++ setup.sh
@@ -13,8 +13,6 @@ apt install -y libx11-dev libfreetype-dev
 
 ./personal.sh
 
-wget https://github.com/be5invis/Iosevka/releases/download/v30.3.2/PkgTTF-IosevkaFixed-30.3.2.zip
-unzip PkgTTF-IosevkaFixed-30.3.2.zip -d Iosevka
 chmod 644 Iosevka/*.ttf
 mv Iosevka/*.ttf /usr/local/share/fonts/
 
